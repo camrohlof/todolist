bc64285efb0556e8c395ee4488758a440ae75dd1
camrohlof <camrohlof@gmail.com>
2023-02-14 15:22:47 -0500

    Switched to clap derive instead of builder

--- src/lib.rs
+++ src/lib.rs
@@ -1,128 +1,169 @@
-use std::process;
+use std::{fmt, process};
 
-use clap::ArgMatches;
+use clap::{Parser, Subcommand};
 use rusqlite::Connection;
 use text_io::read;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about=None)]
+pub struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+pub enum Commands {
+    Add {
+        name: String,
+        details: Option<String>,
+    },
+    Remove {
+        name: String,
+    },
+    Describe {
+        name: String,
+        details: String,
+    },
+    Finish {
+        name: String,
+    },
+}
 
 #[derive(Debug)]
 struct TodoItem {
     id: u32,
-    title: String,
+    name: String,
+    details: String,
+    completed: bool,
 }
 
-pub fn run(matches: ArgMatches, connection: Connection) {
-    let action = handle_input(&matches);
-    match action {
-        Ok(action) => action(connection),
+impl fmt::Display for TodoItem {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "{}: {} | Finished[{}]",
+            self.name, self.details, self.completed
+        )
+    }
+}
+
+pub fn run(cli: Cli, connection: Connection) {
+    match cli.command {
+        Some(commands) => match commands {
+            Commands::Add { name, details } => {
+                add_item(connection, name, details);
+            }
+            Commands::Remove { name } => remove_item(connection, name),
+            Commands::Describe { name, details } => update_item(connection, name, details),
+            Commands::Finish { name } => {
+                complete_item(connection, name);
+            }
+        },
+        None => list_items(connection),
+    };
+}
+
+fn add_item(connection: Connection, name: String, details: Option<String>) {
+    let mut default = "No details provided.".to_owned();
+    if let Some(details) = details {
+        default = details;
+    }
+
+    let result = connection.execute(
+        "INSERT INTO todo (name, details) values (?1,?2)",
+        [&*name, &*default],
+    );
+    match result {
+        Ok(_) => println!("Created todo named: {name}"),
         Err(error) => {
-            eprintln!("{}", error);
+            eprintln!("{error}");
             process::exit(1)
         }
     }
 }
 
-fn handle_input(matches: &ArgMatches) -> Result<fn(Connection), &'static str> {
-    match matches.subcommand_name() {
-        Some("add") => Ok(add_item),
-        Some("remove") => Ok(remove_item),
-        Some("update") => Ok(update_item),
-        Some("list") => Ok(list_items),
-        _ => Err("No matching command found."),
-    }
-}
-
-fn add_item(connection: Connection) {
-    println!("Enter a title for the Todo:");
-    let title: String = read!("{}\n");
-    if !title.trim().is_empty() {
-        let result = connection.execute("INSERT INTO todo (title) values (?1)", [&*title]);
-        match result {
-            Ok(_) => println!("Created todo named: {}", title),
-            Err(error) => {
-                eprintln!("{}", error);
-                process::exit(1)
-            }
-        }
-    } else {
-        eprintln!("Empty string is an invalid title");
-        process::exit(1)
-    }
-}
-
-fn remove_item(connection: Connection) {
-    println!("Enter id of Todo to remove:");
-    let id: u32 = read!();
-    println!("{}", &id);
-    let mut statement = connection.prepare("SELECT * FROM todo WHERE id=?").unwrap();
+fn remove_item(connection: Connection, name: String) {
+    let mut statement = connection
+        .prepare("SELECT * FROM todo WHERE name=?")
+        .unwrap();
     let item = statement
-        .query_row([&id], |row| {
+        .query_row([&name], |row| {
             Ok(TodoItem {
                 id: row.get(0)?,
-                title: row.get(1)?,
+                name: row.get(1)?,
+                details: row.get(2)?,
+                completed: row.get(3)?,
             })
         })
         .unwrap();
 
     println!(
-        "Would you like to remove: [{}: {}]? (Y,n)",
-        item.id,
-        item.title.trim_end(),
+        "Are you sure you would like to remove: [{}]? (Y,n)",
+        item.name.trim_end(),
     );
     let response: String = read!();
     if response.to_lowercase() == "n" || response.to_lowercase() == "no" {
         println!("Exiting.");
         process::exit(1)
     } else {
-        let mut statement = connection.prepare("DELETE FROM todo WHERE id=?").unwrap();
-        let result = statement.execute([item.id.to_string()]).unwrap();
+        let mut statement = connection.prepare("DELETE FROM todo WHERE name=?").unwrap();
+        let result = statement.execute([item.name]).unwrap();
     }
 }
 
-fn update_item(connection: Connection) {
-    println!("Enter id of Todo to modify:");
-    let id: u32 = read!();
-    println!("{}", &id);
-    let mut statement = connection.prepare("SELECT * FROM todo WHERE id=?").unwrap();
+fn update_item(connection: Connection, name: String, details: String) {
+    let mut statement = connection
+        .prepare("SELECT * FROM todo WHERE name=?")
+        .unwrap();
     let item = statement
-        .query_row([&id], |row| {
+        .query_row([&name], |row| {
             Ok(TodoItem {
                 id: row.get(0)?,
-                title: row.get(1)?,
+                name: row.get(1)?,
+                details: row.get(2)?,
+                completed: row.get(3)?,
             })
         })
         .unwrap();
 
-    println!(
-        "Change the title of: [{}: {}]? (Y,n)",
-        item.id,
-        item.title.trim_end(),
-    );
+    println!("Change the details of: [{}]? (Y,n)", item.name.trim_end(),);
     let response: String = read!();
     if response.to_lowercase() == "n" || response.to_lowercase() == "no" {
         println!("Exiting.");
         process::exit(1)
     } else {
         let mut statement = connection
-            .prepare("UPDATE todo SET title=? WHERE id=?")
+            .prepare("UPDATE todo SET details=? WHERE name=?")
             .unwrap();
-        let result = statement
-            .execute([item.title, item.id.to_string()])
-            .unwrap();
+        let result = statement.execute([item.details, item.name]).unwrap();
     }
 }
 
 fn list_items(connection: Connection) {
-    let mut statement = connection.prepare("SELECT * FROM todo").unwrap();
+    let mut statement = connection
+        .prepare("SELECT * FROM todo WHERE completed=False")
+        .unwrap();
     let results = statement
         .query_map([], |row| {
             Ok(TodoItem {
                 id: row.get(0)?,
-                title: row.get(1)?,
+                name: row.get(1)?,
+                details: row.get(2)?,
+                completed: row.get(3)?,
             })
         })
         .unwrap();
     for result in results {
-        let data = result.unwrap();
-        println!("{}: {:?}", data.id, data.title.trim_end())
+        match result {
+            Ok(result) => println!("{result}"),
+            Err(error) => eprintln!("{error}"),
+        }
     }
+}
+
+fn complete_item(connection: Connection, name: String) {
+    let mut statement = connection
+        .prepare("UPDATE todo SET completed=True WHERE name=?")
+        .unwrap();
+    let result = statement.execute([name]).unwrap();
 }
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,18 @@
-use clap::{command, Command};
 use rusqlite::Connection;
-use todolist::run;
-
+use todolist::{run, Cli};
 fn main() {
-    let matches = command!()
-        .subcommand(Command::new("add").about("Add a todo"))
-        .subcommand(Command::new("remove").about("remove a todo"))
-        .subcommand(Command::new("update").about("update a todo"))
-        .subcommand(Command::new("list").about("list a todo"))
-        .get_matches();
+    let cli = <Cli as clap::Parser>::parse();
     let connection = connect_to_database();
-    run(matches, connection);
+    run(cli, connection);
 }
 
 fn connect_to_database() -> Connection {
-    let connection = Connection::open("./todos.db").unwrap();
+    let connection = Connection::open("./todos.sqlite").unwrap();
     let sql = "CREATE TABLE IF NOT EXISTS todo (
         id INTEGER PRIMARY KEY,
-        title TEXT NOT NULL UNIQUE
+        name TEXT NOT NULL UNIQUE,
+        details TEXT,
+        completed BOOLEAN DEFAULT false
     )";
     connection.execute(sql, []).unwrap();
     connection
